ef455220ff4c757463d0fb9cea3d485e27b39000
meme2046 <meme2046@outlook.com>
2026-02-01 10:43:52 +0800

    feat: 添加资源URL映射文件用于七牛云迁移

---
+++ src/tests/to-qiniu.ts
@@ -0,0 +1,141 @@
+export const AVATAR_SRC: Record<string, string> = {
+  "6": "https://arweave.net/gh6dERw6YTSdb7HRYzZYA9WV1dg82J1I3I9fYzmomtU",
+  "34": "https://arweave.net/MdDHWgrX6J-6JgPR5LaFm50mvuFy8z-hLS3QXrRG_j8",
+  "38": "https://arweave.net/WLMz-0yWZhTi_inTmRL4fbyDzVKP1Qk9Tx2VDeFXl4s",
+  "46": "https://arweave.net/giw4W_wu2pXWdSfsBzjvRFcP5JmXLnleVzzOgJhgPzE",
+  "50": "https://arweave.net/OI7oKNP6TeAzCDXCRlfSrS-baom670bhozdEq3kiqUs",
+  "52": "https://arweave.net/Hy9GIxy1dfj0RbVzQCs6MnRDGPfhUtHFUY6bLZE3zb8",
+  "64": "https://arweave.net/A6Wq__dWOYTcIYmPrZyDGWHIwEtzO_oVTzkqfEnMnq8",
+  "65": "https://arweave.net/XU2GHzPO5LdvGsC9FvgJkMt0uV0tUREDYDihibPaszA",
+  "71": "https://arweave.net/M7-A45M71tdpd7p30s1zYtmxmgVZ0Vl8olkpxoRZg1c",
+  "73": "https://arweave.net/RMpV1-nhxoJxbkb7t87_Eg8wCzosomhF8hTey2R44qg",
+  "75": "https://arweave.net/pYfo28vgb1fXvaXdhtVbWeK78WD9_2zPTMoAt45eZzE",
+  "79": "https://arweave.net/YzZB_FCsmpdABmIzgbu0TkiE5PJAM1jfrZNXirkmVN4",
+  "80": "https://arweave.net/uoyT7Lb1zK5KZl7WVJ4-pkLaSoPvSjP377UqjLL16EA",
+  "81": "https://arweave.net/H1xDFDB7naLnRgeRUliVjOKrsQbWB_Uorl_QCtqjbWI",
+  "82": "https://arweave.net/BucAzz46YALpbKjHqsnlAgouDHlQxz6PnNlw2vengpU",
+  "84": "https://arweave.net/XLnzK9JO-Vi6UUGl4FAUSSe2dsHk3PFrlY8uZekir7k",
+  "94": "https://arweave.net/B71iMgEtvHptds7Qde3bQIiCC2kyP8kgUIKZE6RcnGo",
+  "95": "https://arweave.net/rOysZMiykbMV9r1CgkBd1x6hHoHTWWQxwU8Gq7PoU9A",
+  "96": "https://arweave.net/iTHsm2tFuK66u3yj8LvjvWuAL3-Z9mbYJzzOJvnQw2Y",
+  "98": "https://arweave.net/Ziy9tW38o7gGpilNkkhZiakyD9fqRwvOW2WY3IIw1do",
+  "100": "https://arweave.net/_0bEu-PTlOBW6gjEj0Irgdy_sD5QQeml8BsM4Pq9DbY",
+  bot: "https://arweave.net/tcD72fbKvbllrqQdHKaPyx42FlSSDdDqkm8Z1GEAcjs",
+  btc: "https://arweave.net/8eSQ-IslxWW04eTuh2G5vOncXKVLKtXxlKC6vyCemQE",
+  go: "https://arweave.net/__hs2Fm-jF-7yS7MnRkIRVrAdOrdn6iC4-Twxe8e1ps",
+  jks: "https://arweave.net/UPVr-n2J3oijcSx8cCalnNas1KxZVNaHiPmfDxPqTm0",
+  shadcn: "https://github.com/shadcn.png",
+};
+
+export const IMG_SRC: Record<string, string> = {
+  AU888:
+    "https://baidu-finance.cdn.bcebos.com/imgs/icons/0530/futures_ab_GOLD.png",
+  binance: "https://arweave.net/8fUAyHBwxo_1YHybFGN_jI0bTUXW5736AO8vrlVZ0qQ",
+  bitget: "https://arweave.net/_p6Lp4BoYCcCzmKL-nNkQZFotuzgh7WXJuGRFgjWnEE",
+  bitget1: "https://arweave.net/Ki99DzArKbeSS1JtojXj-QyQKZr35s32W8IkRUrDsi8",
+  BTCUSDT: "https://coin-images.coingecko.com/coins/images/1/large/bitcoin.png",
+  coinbase: "https://arweave.net/XCNPLlObUE_n1Taa1iS_9D4_w-KHmXZVfLWI-bMIshA",
+  ETHUSDT:
+    "https://coin-images.coingecko.com/coins/images/279/large/ethereum.png",
+  L1: "https://arweave.net/mJQhNY4Ia1obLHgGvf_DKcAobINcDEm6RbyCbCEnteU", // Level1 基础行情
+  NLS: "https://arweave.net/1A0Jrarna-hux7sirCX0moYedkUqixmJLB1T59Wx6Ew",
+  nodejs: "https://arweave.net/bsMsYyZlCGSZXHuwZ4BReNk1t-xFcg7KYAPHvXBx5jA",
+  okx: "https://arweave.net/zScW1JHznosuXYIQQUjmgAMYekN_esRyJby12ck5UAs",
+  SH: "https://arweave.net/zbcDqHBR69HECkU04pJe2cMb9UZdkaw368Z9-7Ooie0", // 上海证券交易所
+  typescript: "https://arweave.net/ojizDdSfAMkWFEG_L_NyxR4BWTY13MZLjC42WekTA1o",
+  US: "https://arweave.net/yAB9AibGUbrgn0oNiB8Zgw9S0aFwIbnz8gqW52UjM5M",
+  USDCNH: "https://arweave.net/qIGswQTXG_7jNU0RflLi8Qt-2FTC5ccbpWoOmMm7u5Q",
+  WH: "https://arweave.net/Y7ZsOW9u9VZf7fDQLa4tJcjDcACNBXe8L_-PPl5qy6A", // 外汇
+  XAUTUSDT:
+    "https://coin-images.coingecko.com/coins/images/10481/large/Tether_Gold.png",
+  币安人生USDT:
+    "https://coin-images.coingecko.com/coins/images/69848/large/%E5%B8%81%E5%AE%89%E4%BA%BA%E7%94%9F.png",
+};
+
+export const SVG_SRC: Record<string, string> = {
+  "000001": "https://baidu-finance.cdn.bcebos.com/imgs/icons/index_000001.svg",
+  "399300":
+    "https://baidu-finance.cdn.bcebos.com/imgs/logo/indices/d0c11e7e09802a02888fcf5be5af8769.svg",
+  "399905":
+    "https://baidu-finance.cdn.bcebos.com/imgs/logo/indices/62f226489b7e56c90b9e14e0d4fc7531.svg",
+  "600519": "https://arweave.net/K9W8zYbRg6HtBHW1wS273_2td2UOvHS27G9u_Z4ffIc",
+  AG888: "https://arweave.net/cME70JrezZj1FjE-sjkS_QIjMGwWx_U3gV3uTfJPKqQ",
+  ar: "https://arweave.net/CoPs0amqzT8swk9g1S4w_EmRm1YdcBbHFlHzZyLBA20",
+  beta: "https://arweave.net/KCAbhCpxYWfb7mZ3uPF9wQyGyaDYPHwn0zbR7nTXC80",
+  binance: "https://arweave.net/p1KeuJwiAPizd-8DBYAm1_14J3YlsfPu9evxf3M8Cto",
+  bitget: "https://arweave.net/HtmiQ-7AxOkhNtIjKzDBVTlgJv-dCSaDw-TqGeoVQQc",
+  btc: "https://arweave.net/arZDUcYsIe1iaUUiponcag3VY1AHyOII8F6IAavF_Vg",
+  btc1: "https://arweave.net/tKw5i4aX1IxOa89y-ojxFroGjBgxsfkfi1aIsKXeMuY",
+  coinbase: "https://arweave.net/0AL19pT7O6o0P-C_7HrGfn0j1FjGOtNzw6ltMk991BM",
+  eth: "https://arweave.net/CVvypM6_AP45YQKuYkvtSK2RtkeI_iOcfvdDR_WpgK0",
+  fil: "https://arweave.net/56IhfXT0GW8ko-OEOgnsL7y4haqIA1pCFXAc0VwIIiw",
+  gate: "https://arweave.net/xZueBl_L9E1e5N6rNwCC1Q1jdC6Be8viYMncB3d-C0E",
+  IXIC: "https://baidu-finance.cdn.bcebos.com/imgs/logo/indices/bbed2a87be06d1318a5c035e45c0f54c.svg",
+  line: "https://arweave.net/mfUS0iPbauAXqPYb0mRNYDCJhVQn8uKwG_HHqzYhANc",
+  metamask: "https://arweave.net/0ID-EgMR5jZQLj-JGfS2ql1FFaRr-Wvs3VoKRU4VWEU",
+  okx: "https://arweave.net/mbBFb3EXGFjrSG12tLfekPwccVuAVbUd3mEnCUjrt_M",
+  react: "https://arweave.net/NzS63egtcFF4PWAlKouHNqU05BdvWpIdD-TdaI0U2IY",
+  shadcn: "https://arweave.net/-2hSbOI-O2qs3u9d_yf4ylmV3AeSG739b-MaYghh8TA",
+  sol: "https://arweave.net/cGqOWiKeBCH1xO97fbs7Pi4assSD3aBudpuCtamWLy4",
+  solana: "https://arweave.net/cGqOWiKeBCH1xO97fbs7Pi4assSD3aBudpuCtamWLy4",
+  uni: "https://arweave.net/Awt6roS1V9fE9UvVp80x30bYPG-BAmV6Ex-CPz_TXVE",
+  vite: "https://arweave.net/IrKrystwNWLNEto65vp2Uk4LUsSfArXUAheSibRbA80",
+};
+
+export const ICON_SRC: Record<string, string> = {
+  "arrow-down":
+    "https://arweave.net/-YGdFIvjFnQLPSANObNjyEGPFHvlMSMgpRVRGNsh2Tk",
+  "arrow-left":
+    "https://arweave.net/_jJZIrnJMbYX_u4kHjwbHPlEjqTMQnTV4dW6z7c8v0A",
+  "arrow-right":
+    "https://arweave.net/EMScQgcwVo1tRT-OhBgLW6-4ihQW8GrJNlhobHNMnnQ",
+  "arrow-up": "https://arweave.net/RBsQshBEQuJuF8hkyGRVVD3hP02NisXYqZ8oQfzaTG4",
+  chevron: "https://arweave.net/iU7ygwR3S3oAt6EVezM5Ab4l2q5OLwYxCS2G5jTDNsE",
+  copy: "https://arweave.net/OA0m_3us8t5skksCF-w4vT_klbAZvbfb7SRo_cgG7_k",
+  copy1: "https://arweave.net/6GqPMXgkxUEqtzYuJtZ7JlzSbsMNLp_-Me034MneZNw",
+  date: "https://arweave.net/Za54QBVo3UNSk8NpeH5jUdaI1VxYU9SSqH1_30vgHgg",
+  delete: "https://arweave.net/UwaZB8vdRdKX_Qs4rwC-KRP8eoPiANkYFJDyehMy81M",
+  edit: "https://arweave.net/2p7Uk63-5feqnC_J9Cpbs5nO3SvZsA6QEMzoUoqxkPQ",
+  eye: "https://arweave.net/yzGwWAq26yjRpDquLw4-48EtEq6LYnK6YCeAGC1EP8E",
+  home: "https://arweave.net/Pk5G6pbsp4UhkhynGMMvzqbhceU3X4e2IZLb1WouCo4",
+  kill: "https://arweave.net/hXolAnKzsjjlY69gcBxHWUOXFUyUrVqzGe3wPIV38Wc",
+  loading1: "https://arweave.net/ZnecWXdwgPXzNHC4NVOEKYi8T2wRQr8E31MH5C_s8uM",
+  loading2: "https://arweave.net/7Sc0hFWDM0wDHcu8-ArBSYZdyifw2Yk7xXiee7nJbLU",
+  loading3: "https://arweave.net/nNkZ4MZ_w0PwoMrsVX4bqRGbj-qgSDvrHLCDly9H3qA",
+  loading4: "https://arweave.net/FBR8S5bU6VICjCgf4F_7-_xC5OWsrwjqmru4T5wvlJU",
+  menu: "https://arweave.net/n5CWNGfFcKZiG8leIPn8PRZyOgUn02DBJXLG8xT-0sI",
+  menu1: "https://arweave.net/1GP8qNhG-JaAocFryihGI3QRwZ1Pb-eK6OlItqowRQg",
+  menu2: "https://arweave.net/Pozej2qroiuAy5gVRBmXrj-p3ioIz8tghQhHwRKLddo",
+  moon: "https://arweave.net/1MfkG19dwKaxQbA3wxkpSdNyTupuzHa9sXKoIreq3hQ",
+  ok: "https://arweave.net/iErSf5Cjt3Y_pXJxDJgKWBX5D16D2j4nslCrWSc8qpQ",
+  plus: "https://arweave.net/Z1KXoUsfbzVPm0tYBnv9AAmjHqg7EEJV6yM4cHPuJwA",
+  processing: "https://arweave.net/K6C7eMOM7pEESPSgsqR07EP4hUnfz0tQG138M6vpOyY",
+  refresh: "https://arweave.net/HCNMbsyzQDClZPDydPJH3VskQBONC6Ljnds2DWLE_Eo",
+  refresh1: "https://arweave.net/IFrj-BVQlhSrXgaDbmbbCRrGOnCVLKVhAepAwOW0AEs",
+  rocket: "https://arweave.net/S5Zja0AsszmLKAXNEcB0-GOucTcOnxhUVuYyuxW-L-k",
+  search: "https://arweave.net/W8A8Lbrd99c47uFqxvBiDNrGHWSpS8v26IGaGew4KyM",
+  sun: "https://arweave.net/cKUO-f2K_mG5NEt8REnrNBLLxwLE0RELwLqCb0yBCtg",
+  theme: "https://arweave.net/0QLtpZJuOdyvhf3lrW-FbHQ6gfh69pCY1G1fYaGAU9A",
+  "vertical-dots":
+    "https://arweave.net/MCAQ2CskvsJu3Q_Ns9Hcwu8kV20CU0Qj1NTvOB-vLr0",
+  yes: "https://arweave.net/i-hGRM_wFXrpNFJRjvsx4oOzl6v3vlW3WZDEqsbla-4",
+};
+
+// 列表文件支持 csv 和 txt。使用 csv 格式时必须包含表头行，仅支持字段 url（待迁移 url）、key（迁移后的文件名）和 md5（数据内容校验值），仅支持小写且仅 url 为必需。
+// 使用 txt 格式时，每一行表示一个要迁移的文件 URL。
+
+// for (const [key, url] of Object.entries(AVATAR_SRC)) {
+//   console.log(`${url},${key}.png`);
+// }
+
+// for (const [key, url] of Object.entries(IMG_SRC)) {
+//   console.log(`${url},${key}.png`);
+// }
+
+// for (const [key, url] of Object.entries(SVG_SRC)) {
+//   console.log(`${url},${key}.svg`);
+// }
+
+for (const [key, url] of Object.entries(ICON_SRC)) {
+  console.log(`${url},${key}.svg`);
+}
